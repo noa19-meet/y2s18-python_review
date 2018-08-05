72bff5b7a82e6440d851240ca703a8d9e1d299d5
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 11:58:01 +0300

    fun

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,9 @@
 # Write your solution for 1.2 here!
+large=0
+for i in range (1001):
+    if i%6==2:
+        if i**3%5==3:
+            if i>large:
+                large=i
+print (large)
+
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,6 @@
-# Write your solution for 1.1 here!
+# Write your solution for 1.1 h
+sum=0
+for i in range (101):
+    if (i%2==0):
+        sum=sum+i
+print (sum)
